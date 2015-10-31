d015909c243708ac898682dc6420246ae28e23a9
marlib19 <fsm.95.mb@gmail.com>
2015-10-30 21:49:25 -0600

    Ciclos while y do-while

---
+++ while_multiplicacion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int n1;
+int n2;
+int mult;
+int cont;
+int main()
+{
+    printf("Ingrese el primer numero para hacer la multiplicacion \n");
+    scanf("%d", &n1);
+    printf("Ingrese el segundo numero para hacer la multiplicacion \n");
+    scanf("%d", &n2);
+    if(n1>0 && n2>0)
+    {
+            while(cont<n1)
+            {
+                          mult=mult+n2;
+                          cont=cont+1;
+            }
+            printf("El resultado de la multiplicacion es: %d \n", mult);
+    }
+    else
+    {
+        printf("El resultado de la multiplicacion en 0");
+    } 
+}
